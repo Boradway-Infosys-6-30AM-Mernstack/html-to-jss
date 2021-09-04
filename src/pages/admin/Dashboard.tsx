67ffa4106793ebbded89ee0ff618a69295a578ba
RabynCollab <rabyn900@gmail.com>
2021-09-04 17:14:44 +0545

    crud operation added

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,35 +1,131 @@
 import axios from "axios";
+import Interweave from "interweave";
 import React, { useEffect, useState } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, Modal, Form } from "react-bootstrap";
+import ReactQuill from "react-quill"
 import AdminLayout from "../../components/AdminLayout";
+import 'react-quill/dist/quill.snow.css';
 
 interface IArticles {
   id: number;
-  title: string;
-  subtitle: string;
-  content: string;
+  title?: string;
+  subtitle?: string;
+  content?: string;
 }
 
+
+
 const Dashboard = () => {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [showAdd, setShowAdd] = useState(false);
+  const handleShowAdd = () => setShowAdd(true);
+  const handleAddClose = () => setShowAdd(false);
+
   const [data, setData] = useState([] as IArticles[]);
 
-  const fetchData = async () => {
-    const response = await axios.get("http://localhost:3010/articles");
+  const [inputVal, setInputVal] = useState({
+    title: '',
+    subtitle: '',
+    content: ''
+  } as IArticles);
+
+
+  const onEdit = (id: number) => {
+
+    const getDataById = data.find((dat) => dat.id === id);
+
+    setInputVal({
+      title: getDataById?.title,
+      subtitle: getDataById?.subtitle,
+      content: getDataById?.content,
+      id: getDataById?.id,
+    } as IArticles);
+    handleShow();
+
+  }
+
+
+  const addItem = async () => {
+    const response = await axios.post('http://localhost:3010/articles', {
+      title: inputVal.title,
+      subtitle: inputVal.subtitle,
+      content: inputVal.content
+    });
+
+    setInputVal({
+      title: '',
+      subtitle: '',
+      content: ''
+    } as IArticles);
+    if (response.status === 201) {
+      getData();
+
+    }
+    handleAddClose();
+  }
+
+  const getData = async () => {
+    const response = await axios.get('http://localhost:3010/articles');
     setData(response.data);
-  };
-
-  const handleAddBtnClick = () => {
-    console.log("Clicked on add btn");
-  };
-
+  }
   useEffect(() => {
-    fetchData();
+    getData();
   }, []);
+
+
+
+
+
+
+
+
+
+  const updateItem = async (id: number) => {
+    const response = await axios(`http://localhost:3010/articles/${id}`, {
+      method: 'PATCH',
+      data: {
+        title: inputVal.title,
+        subtitle: inputVal.subtitle,
+        content: inputVal.content,
+      },
+    });
+
+    if (response.status === 200) {
+      getData();
+    }
+
+    handleClose();
+
+  }
+
+
+  const removeItem = async (id: number) => {
+    const response = await axios.delete(`http://localhost:3010/articles/${id}`);
+
+    if (response.status === 200) {
+      getData();
+    }
+  }
+
+
+
+
+
+  if (data.length === 0) {
+    return <h1>Loading Please Wait....</h1>
+  }
+
+
+
   return (
     <AdminLayout>
       <div className="head-container">
         <p className="page-title">Articles</p>
-        <Button onClick={handleAddBtnClick} variant="success">
+        <Button variant="success" onClick={handleShowAdd}>
           ADD
         </Button>
       </div>
@@ -49,17 +145,113 @@ const Dashboard = () => {
               <tr key={index}>
                 <td>{article.id}</td>
                 <td>{article.title}</td>
-                <td>{article.subtitle}</td>
-                <td>{article.content}</td>
+                <td> <Interweave content={article.subtitle} /></td>
                 <td>
-                  {" "}
-                  <Button variant="primary">Edit</Button>{" "}
-                  <Button variant="danger">Delete</Button>{" "}
+                  <Interweave content={article.content} />
+                </td>
+                <td>
+                  <Button variant="primary" onClick={() => onEdit(article.id)} >Edit</Button>
+                  <Button variant="danger" onClick={() => removeItem(article.id)}>Delete</Button>
                 </td>
               </tr>
             ))}
           </tbody>
         </Table>
+      </div>
+
+      <Modal show={showAdd} onHide={handleAddClose} >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Article</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              required
+              onChange={(e) => setInputVal(prev => (
+                { ...prev, title: e.target.value }
+              ))}
+              value={inputVal.title}
+              type="text"
+              placeholder="Enter Article's title"
+
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+            <Form.Label>Subtitle</Form.Label>
+            <ReactQuill
+              onChange={(e) => setInputVal(prev => (
+                { ...prev, subtitle: e }
+              ))}
+              value={inputVal.subtitle}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+            <Form.Label>Content</Form.Label>
+            <ReactQuill
+              onChange={(e) => setInputVal(prev => (
+                { ...prev, content: e }
+              ))}
+              value={inputVal.content} />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleAddClose} >
+            Close
+          </Button>
+          <Button variant="primary" onClick={addItem}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Article</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                onChange={(e) => setInputVal(prev => (
+                  { ...prev, title: e.target.value }
+                ))}
+                value={inputVal.title}
+                name="title"
+                type="text"
+                placeholder="Enter Article's title"
+
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label>Subtitle</Form.Label>
+              <ReactQuill
+                onChange={(e) => setInputVal(prev => (
+                  { ...prev, subtitle: e }
+                ))}
+                value={inputVal.subtitle}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+              <Form.Label>Content</Form.Label>
+              <ReactQuill
+                onChange={(e) => setInputVal(prev => (
+                  { ...prev, content: e }
+                ))}
+                value={inputVal.content}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={() => updateItem(inputVal.id)}>
+              Edit Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </AdminLayout>
   );
